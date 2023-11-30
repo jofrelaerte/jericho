89b0f421853ac1f894386c12c9922852d2c143ce
jofrelaerte <128279637+jofrelaerte@users.noreply.github.com>
2023-11-29 19:03:01 -0800

    Add files via upload

---
+++ purecounter3.js
@@ -0,0 +1,119 @@
+       new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.text-sf',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 332193, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 10, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+      new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.text-sf2',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 32748, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 30, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+      new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.substack-sect9-txt1',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 132603, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 10, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+     new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.substack-sect9-txt2',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 40, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 100, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+    new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.substack-sect9-txt3',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 8, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 150, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+    new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.substack-sect9-txt4',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 90594, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 300, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+    new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.substack-sect9-txt5',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 3, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 350, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});

000d2231f3835faf4c54fd74e81ed7092e2435a1
jofrelaerte <128279637+jofrelaerte@users.noreply.github.com>
2023-12-20 14:48:06 -0800

    Add files via upload

---
+++ purecounter_comm3.js
@@ -0,0 +1,53 @@
+ new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.annual-visitors',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 000000, 			            // Starting number [unit]
+    end: 322312, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 10, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+
+   new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.household',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 90594, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 10, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+  
+   new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: '.text-sf',		// HTML query selector for spesific element
+
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, 			            // Starting number [unit]
+    end: 32748, 			            // End number [unit]
+    duration: 2, 	                // The time in seconds for the animation to complete [seconds]
+    delay: 10, 			            // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, 		            // Counting at once or recount when the element in view [boolean]
+    repeat: false, 		            // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, 		            // How many decimal places to show. [unit]
+    legacy: true,                   // If this is true it will use the scroll event listener on browsers
+    filesizing: false, 	            // This will enable/disable File Size format [boolean]
+    currency: false, 	            // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    separator: true, 	            // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
